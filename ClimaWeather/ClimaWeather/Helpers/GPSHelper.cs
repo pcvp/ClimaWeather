8475ac54371f5de6827bec46b87fbc61b96d3f52
Paulo Vieira <paulo.cvieiradev@gmail.com>
2022-06-19 21:35:59 -0300

    Obtendo latitude e longitude

---
+++ ClimaWeather/ClimaWeather/Helpers/GPSHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace ClimaWeather.Helpers {
+    public static class GPSHelper {
+        public static async Task<Location> GetLastKnowLocationAsync() {
+            try {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+
+                if (location != null) {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+                return location;
+            } catch (Exception ex) {
+                await AlertHelper.DisplayAlert("Erro", ex.Message);
+                return null;
+            }
+        }
+
+        public static async Task<Location> GetLocationAsync() {
+            try {
+                var location = await Geolocation.GetLocationAsync();
+
+                if (location != null) {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+                return location;
+            } catch (Exception ex) {
+                await AlertHelper.DisplayAlert("Erro", ex.Message);
+                return null;
+            }
+        }
+    }
+}
