b838eaf02c979b00cf85800998c6058f31628aaf
Paulo Vieira <paulo.cvieiradev@gmail.com>
2022-07-06 21:09:14 -0300

    Adicionar temperaturas por horaio

--- ClimaWeather/ClimaWeather/Converters/GetHourFromUnixTimeConverter.cs
+++ ClimaWeather/ClimaWeather/Converters/GetHourFromUnixTimeConverter.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using ClimaWeather.ExtensionMethods;
+using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Text;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ClimaWeather.Converters {
-
-    public class SomenteInteiroConverter : IValueConverter, IMarkupExtension {
+    public class GetHourFromUnixTimeConverter : IValueConverter, IMarkupExtension {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-            var valor = (double)value;
-            var parteInteira = Math.Truncate(valor);
-            return parteInteira.ToString();
+            var horario = (long)value;
+            return horario.UnixTimeStampToDateTime().ToString("HH:mm");
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
---
+++ ClimaWeather/ClimaWeather/ExtensionMethods/DateExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClimaWeather.ExtensionMethods {
+    public static class DateExtension {
+        public static DateTime UnixTimeStampToDateTime(this long unixTimeStamp) {
+            // Unix timestamp is seconds past epoch
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+    }
+}
