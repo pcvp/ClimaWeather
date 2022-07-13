a08c62d9cac6bb19316883474bad83906cb998bd
Paulo Vieira <paulo.cvieiradev@gmail.com>
2022-07-12 22:27:35 -0300

    Previsão para amanhã

--- ClimaWeather/ClimaWeather/ViewModels/ProximosDias/ProximosDiasViewModel.cs
+++ ClimaWeather/ClimaWeather/ViewModels/ProximosDias/ProximosDiasViewModel.cs
@@ -1,27 +1,27 @@
 ﻿using ClimaWeather.DTOs;
 using ClimaWeather.DTOs.Daily;
 using ClimaWeather.ExtensionMethods;
+using ClimaWeather.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace ClimaWeather.ViewModels.ProximosDias {
-    public class ProximosDiasViewModel {
+    public class ProximosDiasViewModel : BaseViewModel {
+        public ProximosDiasViewModel() {
+            
+        }
+
         public OnecallDTO Clima { get; private set; }
-        public DailyDTO ClimaAmanha { get; set; }
-        public string TextoQuantidadeDeChuvaAmanha { get; private set; }
-        public string TextoVentoAmanha { get; private set; }         
-        public string TextoHumidadeAmanha { get; private set; }
+        public DailyDTO ClimaAmanha => Clima?.Daily?
+            .FirstOrDefault(f => f.Date.UnixTimeStampToDateTime().Date == DateTime.Now.AddDays(1).Date);
+        public string TextoQuantidadeDeChuvaAmanha => string.Concat(ClimaAmanha?.Rain.ToString() ?? "0", "mm");
+        public string TextoVentoAmanha => string.Concat(ClimaAmanha?.WindSpeed.ToString() ?? "0", " km/h");
+        public string TextoHumidadeAmanha => string.Concat(ClimaAmanha?.Humidity.ToString() ?? "0", "%");
 
         public void CarregarDados(OnecallDTO clima) {
             Clima = clima;
-            ClimaAmanha = Clima?.Daily?
-            .FirstOrDefault(f => f.Date.UnixTimeStampToDateTime().Date == DateTime.Now.AddDays(1).Date);
-
-            TextoHumidadeAmanha = string.Concat(ClimaAmanha?.Humidity.ToString() ?? "0", "%");
-            TextoVentoAmanha = string.Concat(ClimaAmanha?.WindSpeed.ToString() ?? "0", " km/h");
-            TextoQuantidadeDeChuvaAmanha = string.Concat(ClimaAmanha?.Rain.ToString() ?? "0", "mm");
         }
 
 
