a17735940d79e792a6729360ef716d59ad407e6c
Paulo Vieira <paulo.cvieiradev@gmail.com>
2022-07-12 21:04:15 -0300

    [WIP] - Página próximos dias

---
+++ ClimaWeather/ClimaWeather/ViewModels/ProximosDias/ProximosDiasViewModel.cs
@@ -0,0 +1,30 @@
+﻿using ClimaWeather.DTOs;
+using ClimaWeather.DTOs.Daily;
+using ClimaWeather.ExtensionMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClimaWeather.ViewModels.ProximosDias {
+    public class ProximosDiasViewModel {
+        public OnecallDTO Clima { get; private set; }
+        public DailyDTO ClimaAmanha { get; set; }
+        public string TextoQuantidadeDeChuvaAmanha { get; private set; }
+        public string TextoVentoAmanha { get; private set; }         
+        public string TextoHumidadeAmanha { get; private set; }
+
+        public void CarregarDados(OnecallDTO clima) {
+            Clima = clima;
+            ClimaAmanha = Clima?.Daily?
+            .FirstOrDefault(f => f.Date.UnixTimeStampToDateTime().Date == DateTime.Now.AddDays(1).Date);
+
+            TextoHumidadeAmanha = string.Concat(ClimaAmanha?.Humidity.ToString() ?? "0", "%");
+            TextoVentoAmanha = string.Concat(ClimaAmanha?.WindSpeed.ToString() ?? "0", " km/h");
+            TextoQuantidadeDeChuvaAmanha = string.Concat(ClimaAmanha?.Rain.ToString() ?? "0", "mm");
+        }
+
+
+
+    }
+}
---
+++ ClimaWeather/ClimaWeather/Views/ProximosDias/ProximosDiasPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ClimaWeather.DTOs;
+using ClimaWeather.ViewModels.ProximosDias;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClimaWeather.Views.ProximosDias {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProximosDiasPage : ContentPage {
+        public ProximosDiasPage(OnecallDTO clima) {
+            InitializeComponent();
+            var vm = (ProximosDiasViewModel)BindingContext;
+            vm.CarregarDados(clima);
+        }
+    }
+}
